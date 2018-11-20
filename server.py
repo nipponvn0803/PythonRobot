30611d627df59011aef2d32db46acf7808b4fa0e
nipponvn0803 <caoson.trinh@gmail.com>
2018-11-20 09:31:55 +0000

    test database connection

---
+++ server.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+import MySQLdb
+
+db = MySQLdb.connect(host="mysql.trinhson.com",    # your host, usually localhost
+                     user="caoson",         # your username
+                     passwd="hikzDFBB",  # your password
+                     db="trinhson_database")        # name of the data base
+
+# you must create a Cursor object. It will let
+#  you execute all the queries you need
+cur = db.cursor()
+
+# Use all the SQL you like
+cur.execute("SELECT * FROM robot_record")
+
+myresult = cur.fetchall()
+
+for x in myresult:
+  print(x)
