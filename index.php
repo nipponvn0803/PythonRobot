d0aed9a4bb8690d1f11d23cb62b89bd1081be7c8
Trinh Son <caoson.trinh@gmail.com>
2018-12-05 10:25:34 +0200

    login

--- index.php
+++ index.php
@@ -1,59 +1,122 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="style.css">
-    <title>Document</title>
-</head>
-<body>
-    <div class="container">
-        <h1>Python Robot</h1>
-        <ul>
-            <li><a href="#home">Home</a></li>
-            <li><a href="#news">News</a></li>
-            <li><a href="#contact">Contact</a></li>
-            <li><a href=""><i class="material-icons">notifications</i></a></li>
-            <li style="float:right"><a href="#about">About</a></li>
-        </ul>
-        <?php
-        include 'credit.php';
-        $servername = "mysql.trinhson.com";
-        $username = $user;
-        $password = $passwd;
-        $database = "trinhson_database";
+<?php
+   ob_start();
+   session_start();
+?>
 
-        // Create connection
-        $conn = new mysqli($servername, $username, $password, $database);
+<?
+   // error_reporting(E_ALL);
+   // ini_set("display_errors", 1);
+?>
 
-        // Check connection
-        if ($conn->connect_error) {
-            die("Connection failed: " . $conn->connect_error);
-        }
+<html lang = "en">
 
-        $sql = "SELECT * FROM robot_record";
-        $result = $conn->query($sql);
-        if ($result->num_rows > 0) {
-            echo "<table><tr><th>ID</th><th>Time</th><th>Name</th></tr>";
-            // output data of each row
-            while($row = $result->fetch_assoc()) {
-                $file_encode = rawurlencode($row['photo_name']);
-                echo "<tr><td>".$row["id"]."</td>
-                <td>".$row["time"]."</td>
-                <td>". " " .'<a href="' . $file_encode . '">'.$row["photo_name"].'</a>'. " " ."</td></tr>";
+   <head>
+      <title>Login</title>
+      <link href = "https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel = "stylesheet">
+
+      <style>
+         body {
+            padding-top: 40px;
+            padding-bottom: 40px;
+            background-color: #ADABAB;
+         }
+
+         .form-signin {
+            max-width: 330px;
+            padding: 15px;
+            margin: 0 auto;
+            color: #017572;
+         }
+
+         .form-signin .form-signin-heading,
+         .form-signin .checkbox {
+            margin-bottom: 10px;
+         }
+
+         .form-signin .checkbox {
+            font-weight: normal;
+         }
+
+         .form-signin .form-control {
+            position: relative;
+            height: auto;
+            -webkit-box-sizing: border-box;
+            -moz-box-sizing: border-box;
+            box-sizing: border-box;
+            padding: 10px;
+            font-size: 16px;
+         }
+
+         .form-signin .form-control:focus {
+            z-index: 2;
+         }
+
+         .form-signin input[type="email"] {
+            margin-bottom: -1px;
+            border-bottom-right-radius: 0;
+            border-bottom-left-radius: 0;
+            border-color:#017572;
+         }
+
+         .form-signin input[type="password"] {
+            margin-bottom: 10px;
+            border-top-left-radius: 0;
+            border-top-right-radius: 0;
+            border-color:#017572;
+         }
+
+         h2{
+            text-align: center;
+            color: #017572;
+         }
+      </style>
+
+   </head>
+
+   <body>
+
+      <h2>Enter Username and Password</h2>
+      <div class = "container form-signin">
+
+         <?php
+            $msg = '';
+
+            if (isset($_POST['login']) && !empty($_POST['username'])
+               && !empty($_POST['password'])) {
+               include 'credit.php';
+               if ($_POST['username'] == $user &&
+                  $_POST['password'] == $passwd) {
+                  $_SESSION['valid'] = true;
+                  $_SESSION['timeout'] = time();
+                  $_SESSION['username'] = 'caoson';
+
+                  echo 'You have entered valid use name and password';
+                  header('Refresh: 2; URL = record.html');
+               }else {
+                  $msg = 'Wrong username or password';
+               }
             }
-            echo "</table>";
-        } else {
-            echo "0 results";
-        }
-        $conn->close();
-        ?>
+         ?>
+      </div> <!-- /container -->
 
-    </div>
+      <div class = "container">
 
+         <form class = "form-signin" role = "form"
+            action = "<?php echo htmlspecialchars($_SERVER['PHP_SELF']);
+            ?>" method = "post">
+            <h4 class = "form-signin-heading"><?php echo $msg; ?></h4>
+            <input type = "text" class = "form-control"
+               name = "username"
+               required autofocus></br>
+            <input type = "password" class = "form-control"
+               name = "password" required>
+            <button class = "btn btn-lg btn-primary btn-block" type = "submit"
+               name = "login">Login</button>
+         </form>
 
+         Click here to clean <a href = "logout.php" tite = "Logout">Session.
 
-</body>
+      </div>
+
+   </body>
 </html>
